2f767b819372eab2a2463087f19f55bde6a0f38d
Fernandorec <fmgonzalez644@gmail.com>
2026-06-20 09:17:23 -0600

    Intento parte 2 parciall

--- RegistroAtencion.py
+++ RegistroAtencion.py
@@ -7,7 +7,7 @@ class RegistroAtencion:
         self.carnet = carnet
         self._recursos = []
         self.bibliotecario = bibliotecario
-        self.multas = int
+        self.multas = []
 
     def agregar_recurso(self, recurso):
         if len(self._recursos) >= 4:
@@ -16,18 +16,18 @@ class RegistroAtencion:
 
     def obtener_recursos(self):
         return self._recursos.copy()
-    
 
-    
+
+
     def __str__(self):
         return f"{self.codigo}, {self.carnet}, {self.bibliotecario}"
 
 
 
     def revision_Saldos(self, multas):
-        self.multas = sum(self.multas)
-    
-    
-    def Obtener_multas(self)
-        if ((sum(self.multas)* 100% / 15)) >= 15 :
+        self.multas = multas
+
+
+    def Obtener_multas(self):
+        if (sum(self.multas) * 100 / 15) >= 15:
             raise ValueError("CUENTA_SUSPENDIDA")
